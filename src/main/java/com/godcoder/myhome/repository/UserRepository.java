ff1e9ce18c0dcfd94fb8d0f5798cfa525ac7b714
whowho0820 <whowho0820@naver.com>
2020-10-11 20:57:40 +0900

    로그인 처리중

---
+++ src/main/java/com/godcoder/myhome/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.godcoder.myhome.repository;
+
+import com.godcoder.myhome.model.Board;
+import com.godcoder.myhome.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+}
--- src/main/java/com/godcoder/myhome/validator/BoardValidator.java
+++ src/main/java/com/godcoder/myhome/validator/BoardValidator.java
@@ -3,6 +3,7 @@ package com.godcoder.myhome.validator;
 import com.godcoder.myhome.model.Board;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.Errors;
+import org.springframework.validation.ValidationUtils;
 import org.springframework.validation.Validator;
 import org.thymeleaf.util.StringUtils;
 
@@ -16,6 +17,9 @@ import org.thymeleaf.util.StringUtils;
     @Override
     public void validate(Object obj, Errors errors) {
         Board b = (Board) obj;
+//        if(StringUtils.isEmpty(b.getTitle())) {
+//            errors.rejectValue("title", "key", "제목을 입력하세요");
+//        }
          if(StringUtils.isEmpty(b.getContent())) {
          errors.rejectValue("content", "key", "내용을 입력하세요");
          }
